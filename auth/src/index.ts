8f30426674907a4338f60db4d09b75fb4e0b6efb
DNZ8\deniz <denizgirginer@gmail.com>
2021-01-03 14:15:13 +0300

    gce v7

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('starting up...gke v6');
+  console.log('starting up...gke v7');
   
   if(!process.env.JWT_KEY){
     throw new Error('JWT_KEY not defined');
