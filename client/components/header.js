89ef2faaa0fda97b17b783a9632412f0393e96ef
DNZ8\deniz <denizgirginer@gmail.com>
2021-01-03 14:59:13 +0300

    client

--- client/components/header.js
+++ client/components/header.js
@@ -19,7 +19,7 @@ const Header = ({currentUser})=> {
 
     return <nav className="navbar navbar-ligh bg-light">
         <Link href="/">
-            <a className="navbar-brand">GitTix</a>
+            <a className="navbar-brand">GitTix v1</a>
         </Link>
 
         <div className="d-flex justify-content-end">
