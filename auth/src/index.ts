0e639a085cbb18e41595a98e6e85beaf2dc8dc1a
DNZ8\deniz <denizgirginer@gmail.com>
2021-01-06 10:56:56 +0300

    auth pvc

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('starting up gce....');
+  console.log('starting up gce.....');
   
   if(!process.env.JWT_KEY){
     throw new Error('JWT_KEY not defined');
